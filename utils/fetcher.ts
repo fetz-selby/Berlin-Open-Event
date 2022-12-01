47a7f687be17ca1e154281f25bf9cd5d331bc946
Emmanuel Selby <fetz.selby@gmail.com>
2022-12-01 15:01:12 +0100

    Update fetcher.ts
    cleanup

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,4 +1,3 @@
-//I'm grabbing the data from the API and storing it in the variable data
 const URL =
   "https://www.berlin.de/sen/web/service/maerkte-feste/wochen-troedelmaerkte/index.php/index/all.json?q=";
 
